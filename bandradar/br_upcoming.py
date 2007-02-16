dd394825d6dca01e9200e7b6fee40c21724f0cae
Andy Grover <andy@bandradar.com>
2007-02-16 14:07:44 -0800

    allow time to be empty

--- bandradar/br_upcoming.py
+++ bandradar/br_upcoming.py
@@ -18,15 +18,17 @@ def events():
         for event in events:
             if not (int(event.personal)):
                 event_date = date(*[int(num) for num in event.start_date.split('-')])
-                hrs, mins = [int(num) for num in event.start_time.split(':')[:2]]
-                suff = "am"
-                if hrs > 12:
-                    hrs -= 12
-                    suff = "pm"
-                event_time = unicode(hrs)
-                if mins:
-                    event_time += ":" + unicode(mins)
-                event_time += " " + suff
+                event_time = None
+                if event.start_time:
+                    hrs, mins = [int(num) for num in event.start_time.split(':')[:2]]
+                    suff = "am"
+                    if hrs > 12:
+                        hrs -= 12
+                        suff = "pm"
+                    event_time = unicode(hrs)
+                    if mins:
+                        event_time += ":" + unicode(mins)
+                    event_time += " " + suff
                 event_description = event.description
                 try:
                     ev = api.venue.getInfo(venue_id=int(event.venue_id))[0]
@@ -39,3 +41,8 @@ def events():
                            description=clean(event.description),
                            artists=clean(event.name).split(','),
                            venue=venue)
+
+if __name__ == "__main__":
+    for event in events():
+        print event['name'], event['time']
+
