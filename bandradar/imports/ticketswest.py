4f509b4bee39011a8d807669e68a8a55735ee22a
Andy Grover <andy@bandradar.com>
2007-04-02 15:34:15 -0700

    add ticketwest support

---
+++ bandradar/imports/ticketswest.py
@@ -0,0 +1,36 @@
+from BeautifulSoup import BeautifulSoup
+import urllib
+import datetime
+import time
+import re
+
+venue_list = (("Doug Fir", "DFL"), ("Roseland", "ROS"))
+
+base_url = "http://ticketswest.rdln.com"
+page_url = "/Venue.aspx?ven="
+
+def events():
+    #for venue_name, code in venue_list:
+    usock = urllib.urlopen(base_url + page_url + "DFL")
+    soup = BeautifulSoup(usock.read())
+    table = soup("table", attrs={"class":"eventDataGrid"})[0]
+    trs = table("tr")[1:-1]
+    venue_name = "doug fir"
+    venue = dict(name=venue_name)
+    for tr in trs:
+        event = dict(source="ticketswest")
+        name_td = tr("td", attrs={"class":"borderTopRight paddedLeft"})[0]
+        event['artists'] = name_td.a.string.split("*")[0].split(",")
+        event['name'] = ", ".join(event['artists'])
+        date_td = tr("td", attrs={"class":"borderTopRight"})[0]
+        weekday, date_str, time_str, ampm =  date_td.string.strip().split()
+        event['date'] = datetime.date(*time.strptime(date_str, "%m/%d/%y")[:3])
+        event['time'] = time_str + " " + ampm
+        link_td = tr("td")[3]
+        event['ticket_url'] =  base_url + link_td.div.a.attrs[1][1]
+        event['venue'] = venue
+        yield event
+
+if __name__ == "__main__":
+    result = list(events())
+    print len(result)
