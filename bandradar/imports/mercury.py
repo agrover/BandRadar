9d51b72a87504f6504573b3d14fb8127c932b9e9
Andy Grover <andy@groveronline.com>
2009-01-19 23:53:20 -0800

    update mercury for current website

--- bandradar/imports/mercury.py
+++ bandradar/imports/mercury.py
@@ -11,29 +11,41 @@ import logging
 log = logging.getLogger("bandradar.controllers")
 
 def date_to_url(date):
-    baseurl = "http://www.portlandmercury.com/portland/FoundIt?search=music&musicBand1=&musicBand2=&musicCategory=&musicLocation=&musicStartDate="
+    baseurl = "http://www.portlandmercury.com/gyrobase/EventSearch?eventCategory=807942&eventSection=807941&narrowByDate="
     return baseurl + date.strftime("%Y-%m-%d")
 
+def event_urls_for_date(date):
+    page = 1
+    while True:
+        soup = import_util.url_to_soup(date_to_url(date) + "&page=%s" % page)
+        events = soup.findAll("div", "EventListing clearfix")
+        if not len(events):
+            break
+        for event in events:
+            yield event.div.h3.find("a", recursive=False)['href']
+        page += 1
+
 def day_events(date):
-    soup = import_util.url_to_soup(date_to_url(date))
-    events = soup.findAll("div", "event_group ")
-    events.extend(soup.findAll("div", "event_group staffpick_music"))
-    for event_div in events:
+    for url in event_urls_for_date(date):
+        soup = import_util.url_to_soup(url)
         event = dict(source="mercury")
         event['date'] = date
-        event['venue'] = dict(name=event_div.h4.a.string)
+
+        lt = soup.find("h1", "listingTitle")
+        crap = lt.find("div", "FeaturesIcons")
+        if crap:
+            crap.extract()
+        event['name'] = import_util.stringify(lt).strip()
+
+        el = soup.find("div", id="EventLocation")
+        event['venue'] = dict(name=el.ul.li.h4.find("a", recursive=False).string)
         event['artists'] = []
-        for li in event_div.find("ul", "event_musicians").findAll("li"):
-            artist_name = "".join([x.string for x in li.contents if x.string]).strip()
-            event['artists'].append(artist_name)
-        if not len(event['artists']):
-            continue
-        if event_div.h5:
-            event['name'] = import_util.stringify(event_div.h5).strip(":")
-        else:
-            event['name'] = ", ".join(event['artists'])
-        if event_div.p.strong:
-            event['cost'] = event_div.p.strong.string
+
+        artists_str = event['name']
+        if artists_str.find(":") != -1:
+            event['name'], artists_str = artists_str.split(":", 1)
+        event['artists'] = artists_str.split(",")
+        event['artists'] = [a.strip() for a in event['artists']]
         yield event
 
 def week_events(start_date):
@@ -42,7 +54,7 @@ def week_events(start_date):
             yield event
 
 if __name__ == "__main__":
-    for event in day_events(datetime.date.today() - datetime.timedelta(1)):
+    for event in day_events(datetime.date.today()):
         print "Venue: " + event['venue']['name']
         print "  " + event.get("name", "no event name")
         for artist in event['artists']:
